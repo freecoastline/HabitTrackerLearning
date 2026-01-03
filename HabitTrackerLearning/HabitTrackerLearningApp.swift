4ddc9c1990e03ac8afb084c2919ef4e18a137b91
ken <jitamm20081@gmail.com>
2026-01-03 23:49:33 +0800

    Initial Commit

---
+++ HabitTrackerLearning/HabitTrackerLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerLearningApp.swift
+//  HabitTrackerLearning
+//
+//  Created by ken on 2026/1/3.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
